9425fd3acf0d46559bd91c5830f9f2ed3e56a3cd
Ilya Mironov <koiiiara@me.com>
2019-12-15 16:52:28 +0300

    Initial Commit

---
+++ Rem Vend/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Rem Vend
+//
+//  Created by Илья Миронов on 15.12.2019.
+//  Copyright © 2019 Ilya Mironov. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
